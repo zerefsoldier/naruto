97800cd8e92680073ffee17d752e93704830afa4
Marc CHANTEBEL <chantebelmarc@gmail.com>
2015-11-12 12:15:33 +0100

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,1 +1,10 @@
-
+<?php
+  // récupération des infos du joueur pour le combat : dans un premier temps en base. Dans un second temps ce processus sera fait directement lors de la connexion du client
+  $pdo = new PDO("chaine");
+  //Selectionner les infos de base du joueur
+  //prendre ensuite ses armes
+  //prendre ensuite ses armures
+  //prendre ensuite ses objets divers
+  //prendre ensuite ses sacs
+  //prendre ensuite ses techniques de combat
+  
