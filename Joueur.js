fd00fee055a7fc07deb71db13734fd92ce560a64
Marc CHANTEBEL <chantebelmarc@gmail.com>
2015-11-12 12:31:00 +0100

    Update Joueur.js

--- Joueur.js
+++ Joueur.js
@@ -1,1 +1,8 @@
-var 
+var Joueur = (function(){
+  Joueur.prototype.joueur = function(datas){
+    var datasTemp = datas.split("*");
+    this.nom = datasTemp[0];
+    this.village = datasTemp[1];
+    //this. = datasTemp[2];
+  };
+});
